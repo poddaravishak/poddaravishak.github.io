fb3bd46e26d61d310bc3237067b0a81472110e43
Avishak_poddar <40418626+poddaravishak@users.noreply.github.com>
2023-03-28 14:00:22 +0600

    test

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
+            
             $('.navbar').removeClass("sticky");
         }
         
